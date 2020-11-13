82a8f917c8c195f686fa6297c11c404e27ff5154
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2020-11-13 20:30:59 -0300

    Primeiro commit

---
+++ javascript_dio_fundamentos_aritmeticos.js
@@ -0,0 +1,38 @@
+/*let entrada = gets().split(" ");
+let H = parseInt(entrada[0]);
+let P = parseInt(entrada[1]);
+let media = (H/P).toFixed(2);
+console.log(" "+ media)*/
+/*let entrada = gets().split(" ")
+ let tempo = parseInt(entrada[0])
+ let vm = parseInt(entrada[1])
+ let distancia = (tempo*vm).toFixed(2)
+ let litros = (distancia/12).toFixed(3)
+ console.log(" "+ litros) */
+
+/*/desafio 4/
+ let entrada = gets()
+ if(entrada<=2000){
+   console.log("Isento")
+ }
+ 
+ else if (entrada<=3000){
+   segunda_faixa = ((entrada-2000)*0.08).toFixed(2);
+   total=segunda_faixa;
+    console.log("R$ "+ total)
+ }
+ 
+ else if (entrada<=4500){
+   segunda_faixa = (3000-2000)*0.08;
+   terceira_faixa = (entrada-3000)*0.18;
+   total=(segunda_faixa+terceira_faixa).toFixed(2)
+   console.log("R$ "+ total)
+ }
+ 
+ else if(entrada>4500){
+   segunda_faixa = (3000-2000)*0.08;
+   terceira_faixa = (4500-3000)*0.18;
+   quarta_faixa = (entrada - 4500)*0.28;
+   total = (segunda_faixa+terceira_faixa+quarta_faixa).toFixed(2);
+    console.log("R$ "+ total)
+ }*/
